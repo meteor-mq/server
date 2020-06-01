4a36ea3c0975ab97592c1d6d224f875f52ca6e34
Dings <deen.job@qq.com>
2020-06-01 22:43:39 +0800

    add file

--- command/cmd_parse.go
+++ command/cmd_parse.go
@@ -1,5 +1,12 @@
 package command
 
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"go.coder.com/cli"
+)
+
 // HelpCmd help command
 type HelpCmd struct {
 	// This root command has no default action, so print the help.
@@ -11,4 +18,45 @@ type argsCmd struct {
 	cfgPath string
 	// Whether to run in the background
 	isBackground bool
+}
+
+func (c *argsCmd) Run(fl *pflag.FlagSet) {
+	if c.isBackground {
+		fmt.Println("application is background.")
+	}
+}
+
+// GetHelpCmd get help command root
+func GetHelpCmd() *HelpCmd {
+	return &HelpCmd{}
+}
+
+func (c *argsCmd) Spec() cli.CommandSpec {
+	return cli.CommandSpec{
+		Name:  "-back",
+		Usage: "",
+		Desc:  `Whether to run in the background.`,
+	}
+}
+
+type cmd struct {
+}
+
+func (c *HelpCmd) Run(fl *pflag.FlagSet) {
+	// This root command has no default action, so print the help.
+	fl.Usage()
+}
+
+func (c *HelpCmd) Spec() cli.CommandSpec {
+	return cli.CommandSpec{
+		Name:  "FastMQ",
+		Usage: "[-help]",
+		Desc:  `Use the  command to view the help message.`,
+	}
+}
+
+func (c *HelpCmd) Subcommands() []cli.Command {
+	return []cli.Command{
+		&argsCmd{},
+	}
 }
